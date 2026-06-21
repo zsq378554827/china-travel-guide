46c4cc54959a288d512bb2c1c97e93f46cd9dfb0
zsq378554827 <378554827@qq.com>
2026-06-21 15:03:28 +0800

    Add homepage map concept previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || "https://china-travel-guide-bq8.pages.dev",
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => !/\/map-concept-(a|b|c)\/?$/.test(page),
+    }),
+  ],
 });
